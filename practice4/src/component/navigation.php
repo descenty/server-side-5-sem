76348c2a70723add1ee0df5e898416ac762ddc55
Александр <bychenkov.a.k@edu.mirea.ru>
2023-09-25 12:00:59 +0000

    complete practice4

--- practice4/src/component/navigation.php
+++ practice4/src/component/navigation.php
@@ -1,13 +1,17 @@
-<?php 
-  function navigation() {
-    return '
+<?php
+function navigation()
+{
+  return '
         <nav>
             <a href="index.php">Товары</a>
             <a href="readOne.php">Найти товар</a>
             <a href="create.php">Создать товар</a>
             <a href="update.php">Редактировать товар</a>
             <a href="delete.php">Удалить товар</a>
+            <br />
+            <a href="delivery.html">Доставка</a>
+            <a href="contacts.html">Контакты</a>
         </nav>
     ';
-  }
+}
 ?>
