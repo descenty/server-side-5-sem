8b57fbf5c52b1cf9414e38b743b627f764f13e4b
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-10-04 04:18:15 +0300

    add gson

--- practice5/src/main/java/com/mirea/practice5/Practice5Application.java
+++ practice5/src/main/java/com/mirea/practice5/Practice5Application.java
@@ -2,8 +2,9 @@ package com.mirea.practice5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { JacksonAutoConfiguration.class }) // Exclude the automatic configuration of JACKSON
 public class Practice5Application {
 
 	public static void main(String[] args) {
