25df8a2a67745db6494946ef1d6b7c8466285c3b
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-10-25 15:48:52 +0300

    almost complete practice6 report

--- practice6/src/main/java/com/mirea/practice6/core/schema/in/ProductCreateDTO.java
+++ practice6/src/main/java/com/mirea/practice6/core/schema/in/ProductCreateDTO.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record ProductCreateDTO(@NotNull String name, @NotNull String description, @Min(1) BigDecimal price,
+public record ProductCreateDTO(
+        @NotNull String name,
+        @NotNull String description,
+        @Min(1) BigDecimal price,
         @Min(1) Integer stockQuantity) {
 }
